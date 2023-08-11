0267072a9a487ccf4bc37d75b3ad1d4ccc36f522
Dineshpandi1320 <dineshd1307@gmail.com>
2023-08-11 14:28:49 +0530

    backend

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/fitness_logger"
+
+//
+// const URI = process.env.MONGODB_URL;
+
+// mongoose.connect(URI, {
+
+// useNewUrlParser: true, 
+
+// useUnifiedTopology: true 
+
+// }, 
+
+// const mongoose = require('mongoe');
+// const mongoURI = "mongodb://localhost:27017"
+
+// const connectToMongo = async () => {
+// try {
+//     mongoose.set('strictQuery', false)
+//     mongoose.connect(mongoURI) 
+//     console.log('Mongo connected')
+// }
+// catch(error) {
+//     console.log(error)
+//     process.exit()
+// }
+// }
+// module.exports = connectToMongo;
+
+// const uri = process.env.MONGODB_URL;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+// );
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log("MongoDB database connection established successfully");
+// })
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+const connection=async()=>{
+        await mongoose.connect(mongoURI, {
+        useNewUrlParser:true
+    })
+    console.log("Mongodb is connected")
+    }
+    connection()
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
